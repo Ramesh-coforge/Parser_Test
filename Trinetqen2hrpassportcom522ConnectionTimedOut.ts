d3e930d076d649098fcb3e0182a6297f910d131b
Mudavath Ramesh <Mudavath.Ramesh@coforge.com>
2026-01-14 13:21:44 +0530

    'fix'

---
+++ Trinetqen2hrpassportcom522ConnectionTimedOut.ts
@@ -0,0 +1,15 @@
+import { expect, type Locator, type Page } from '@playwright/test';
+
+export class Trinetqen2hrpassportcom522ConnectionTimedOut {
+    readonly page: Page;
+
+    constructor(page: Page) {
+        this.page = page;
+    }
+
+    // Description : Launch Application
+    public async launchApplication(): Promise<void> {
+        // No actions
+    }
+
+}
